99fb4eb94a5b2ebded2d18ed2a65356beab7b1cc
mubashir-73 <mubashirsheriff7@gmail.com>
2025-01-01 21:50:05 +0530

    Created basic design of todo

---
+++ todo-app/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <>
+      <header className="menu">
+        <ul className="List">
+          <li>Welcome</li>
+          <li>Tasks</li>
+        </ul>
+      </header>
+    </>
+  );
+}
---
+++ todo-app/src/components/todolist.jsx
@@ -0,0 +1,11 @@
+export default function Todo() {
+  return (
+    <div className="todo">
+      <h1>TO-DO</h1>
+      <hr className="todosep" />
+      <div className="butts">
+        <button className="add">+ New Task</button>
+      </div>
+    </div>
+  );
+}
