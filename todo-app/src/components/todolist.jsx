656c37a6ac85034084ee18a01ba1e6b5b881a0aa
mubashir-73 <mubashirsheriff7@gmail.com>
2025-01-04 11:26:05 +0530

    Implement minor design changes

--- todo-app/src/components/todolist.jsx
+++ todo-app/src/components/todolist.jsx
@@ -1,22 +1,17 @@
-import { useEffect } from "react";
-import Todocreate from "./addtodo";
-
 export default function Todo() {
-  useEffect(() => {
-    Todocreate();
-  }, []);
   return (
     <div className="todo">
       <h1>TO-DO</h1>
       <hr className="todosep" />
-      <div className="butts">
-        <button className="add">+ New Task</button>
+      <form className="forms">
         <input
           type="text"
           id="myInput"
-          placeholder="Enter name of the task here"
+          placeholder="e.g. reading a book"
+          aria-label="Add Tasks"
         />
-      </div>
+        <button className="add">+ New Task</button>
+      </form>
       <div className="card">
         <ul className="myUL"></ul>
       </div>
